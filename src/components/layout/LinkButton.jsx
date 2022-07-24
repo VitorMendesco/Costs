7b3df36aa381aba8fbec2f5320e6a004c92eb98e
VitorMendesco <vitor.mcosta@sptech.school>
2022-07-24 12:22:56 -0300

    start projects dashboard

--- src/components/layout/LinkButton.jsx
+++ src/components/layout/LinkButton.jsx
@@ -1,13 +1,22 @@
 // react
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
 
 // css
 import styles from "./LinkButton.module.css";
 
-export default function LinkButton({to, text}) {
-    return (
-        <Link to={to} className={styles.btn} draggable={false}>
-            {text}
-        </Link>
-    );
+export default function LinkButton({ to, text }) {
+  return (
+    <Link to={to} className={styles.btn} draggable={false}>
+      {text}
+    </Link>
+  );
 }
+
+LinkButton.propTypes = {
+  to: PropTypes.string.isRequired,
+  text: PropTypes.string,
+};
+LinkButton.defaultProps = {
+  text: "Create Project",
+};
--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -2,7 +2,12 @@
 import { useLocation } from "react-router-dom";
 
 // components
+import Container from "../layout/Container";
 import Message from "../layout/Message";
+import LinkButton from "../layout/LinkButton";
+
+// css
+import styles from "./Projects.module.css";
 
 export default function Projects() {
   const location = useLocation();
@@ -12,9 +17,15 @@ export default function Projects() {
   }
 
   return (
-    <div>
-      <h1>Projects</h1>
+    <div className={styles.project_container}>
+      <div className={styles.title_container}>
+        <h1>Projects</h1>
+        <LinkButton to="/newproject" text="New Project" />
+      </div>
       {message && <Message msg={message} type="success" />}
+      <Container customClass="start">
+        <p>Projects...</p>
+      </Container>
     </div>
   );
 }
